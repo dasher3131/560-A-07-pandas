2bf3c0b131b7a7acd56c37ef223d700f52f951c3
dasher3131 <dasher31@unc.edu>
2025-11-11 17:31:06 -0500

    commit 0

--- roster.py
+++ roster.py
@@ -1,5 +1,6 @@
 # https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
 roster = ["Evans", "High", "Brown"]
-for player in roster:
-    print(player)
+data = pd.DataFrame(roster)
+print(data)
